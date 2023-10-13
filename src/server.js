c74cc8482293f0e291f5031e4a252486be61a7af
TranPhanPhucAn <21120407@student.hcmus.edu.vn>
2023-10-13 23:32:53 +0700

    init backend

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import configViewEngine from "./configs/viewEngine";
+import initWebRoutes from "./routes/web";
+require("dotenv").config();
+const app = express();
+//config view engine
+configViewEngine(app);
+//init web route
+initWebRoutes(app);
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log("jwt");
+});
